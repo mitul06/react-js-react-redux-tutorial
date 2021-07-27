3db9f9238eb0e54987ad5376dc345e8514786f37
Mitul Sartanpara <mitulsartanpara0607@gmail.com>
2021-07-27 19:07:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions";
 
-function App() {
+const App = () => {
+  const changeTheNumber = useSelector((state) => state.changeTheNumber);
+
+  const dispatch = useDispatch();
+
+  const handleChange = () => {
+    console.log("Hello");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-dv">
+        <div className="container">
+          <h1>Increment / Decrement Counter</h1>
+          <h4>using React and Redux</h4>
+
+          <div className="quantity">
+            <a
+              className="quantity__minus"
+              title="Decrement"
+              onClick={() => dispatch(decNumber(5))}
+            >
+              <span>-</span>
+            </a>
+            <input name="quantity" type="text" className="quantity__input" value={changeTheNumber} onChange={handleChange}/>
+            <a
+              className="quantity__plus"
+              title="Increment"
+              onClick={() => dispatch(incNumber(5))}
+            >
+              <span>+</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import changeTheNumber from "./upDown";
+
+import { combineReducers } from "redux";
+
+const reducers = combineReducers(
+    {
+    //   myNumber:changeTheNumber
+        changeTheNumber
+    }
+);
+
+export default reducers;
